58829af1f8919f5b32bae223e411d9c1f802b5fb
imeian <imeian@gmail.com>
2019-08-27 16:55:57 +0200

    added storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import { Button, Welcome } from '@storybook/react/demo'
+
+import Connecting from '../src/components/spinners/Connecting'
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
+
+storiesOf('Button', module)
+
+  .add('with text', () => 
+    <Button onClick={action('clicked')}>Hello Button lel</Button>
+  )
+
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))
+
+  .add('Connecting', () => 
+    <Connecting />
+  )
